8d6eda4ca0d0b4e4e39c7fee05227292a0048cd2
Josh Painter <jpainter4@gmail.com>
2014-12-30 23:18:43 -0800

    Added sound, animation

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
-	$(".ryu").mouseenter(function() {
-		$(".ryu-still").hide();
-		$(".ryu-ready").show();
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
 	});
-	$(".ryu").mouseleave(function() {
-		$(".ryu-ready").hide();
-		$(".ryu-still").show();
+	$('.ryu').mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
 	});
-	$(".ryu").mousedown(function() {
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").show();
-		$(".hadouken").show();
+	$('.ryu').mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show();
+		$('.hadouken').animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
 	});
-	$(".ryu").mouseup(function() {
+	$('.ryu').mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
